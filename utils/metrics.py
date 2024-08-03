b4e4c805b632bd5e8e07ed1f5ed9a3ed3419cb76
贵大孙波2002 <sky200210@outlook.com>
2024-08-03 09:42:17 +0800

    fix a bug

--- utils/metrics.py
+++ utils/metrics.py
@@ -52,6 +52,22 @@ def MAPE(preds, labels, null_val: float = np.nan):
     loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
     return torch.mean(loss)
 
+def masked_mape_torch(preds, labels, null_val=np.nan, mask_val=np.nan):
+    labels[torch.abs(labels) < 1e-4] = 0
+    if np.isnan(null_val):
+        mask = ~torch.isnan(labels)
+    else:
+        mask = labels.ne(null_val)
+    if not np.isnan(mask_val):
+        mask &= labels.ge(mask_val)
+    mask = mask.float()
+    mask /= torch.mean(mask)
+    mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
+    loss = torch.abs((preds - labels) / labels)
+    loss = loss * mask
+    loss = torch.where(torch.isnan(loss), torch.zeros_like(loss), loss)
+    return torch.mean(loss)
+
 
 def MSPE(preds, labels, null_val: float = np.nan):
     """Masked mean square percentage error.
@@ -90,7 +106,8 @@ def metric(pred, true):
     mae = masked_mae(pred, true, 0.0)
     mse = MSE(pred, true, 0.0)
     rmse = RMSE(pred, true, 0.0)
-    mape = MAPE(pred, true)
+    # mape = MAPE(pred, true)
+    mape = masked_mape_torch(pred, true)
     mspe = MSPE(pred, true)
 
     return mae, mse, rmse, mape, mspe
