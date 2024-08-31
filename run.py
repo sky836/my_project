ba4c5b68d2aad7fb4fdbd38552e9f53626481454
贵大孙波2002 <sky200210@outlook.com>
2024-08-31 19:33:49 +0800

    fix a bug

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 
     # path to modify
     # 1. data and adj
-    parser.add_argument('--adj_path', type=str, default=r'datasets/PEMSBAY/adj_PEMS-BAY.pkl', help='path of the adjmx')
+    parser.add_argument('--adj_path', type=str, default=r'/kaggle/input/traffic-datasets/datasets/PEMSBAY/adj_PEMS-BAY.pkl', help='path of the adjmx')
     parser.add_argument('--root_path', type=str, default='/kaggle/input/traffic-datasets/datasets/', help='root path of the data file')
     parser.add_argument('--data_path', type=str, default='PEMSBAY/data.npz', help='data file')
     parser.add_argument('--data', type=str, required=False, default='PEMSBAY', help='dataset type, [Pretrain_Forecast, Pretrain_Class]')
